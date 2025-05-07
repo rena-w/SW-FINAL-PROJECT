4953fa5a9d6e92066869ed7aa04ef2d523a6c58a
Serena Wong <199941651+rena-w@users.noreply.github.com>
2025-05-06 23:05:20 -0400

    tried to find the weird category 'Great' -- didn't work

--- finalproject.py
+++ finalproject.py
@@ -12,18 +12,18 @@ def get_lines(file): # get data in form of lines
         lines_raw = [line.split() for line in lines] # this gets us each article entry in a separate string
         joined_lines = [' '.join(lin) for lin in lines_raw]
         chars = str.maketrans({'{': '', '}': '', ',': '', '\"':'','\"':'', '\'':'', '-':' '})
-        clean = [item.translate(chars) for item in joined_lines] # gets rid of punctuation like {} brackets and double quotes ""
-        return clean
+        cleaned = [item.translate(chars) for item in joined_lines] # gets rid of punctuation like {} brackets and double quotes ""
+        return cleaned
     
-    clean = extract_lines(file)   
+    clean = extract_lines(file)  
     
     def replace_cats(list): # replace category labels
         a = [i.replace('link: ', '$A') for i in list]
-        b = [al.replace('headline: ', ':::$B') for al in a]
-        c = [bl.replace('category: ', ':::$C') for bl in b]
-        d = [cl.replace('short_description: ', ':::$D') for cl in c]
-        e = [dl.replace('authors: ', ':::$E') for dl in d]
-        f = [el.replace('date: ', ':::$F') for el in e]
+        b = [al.replace('headline: ', '::: $B') for al in a]
+        c = [bl.replace('category: ', '::: $C') for bl in b]
+        d = [cl.replace('short_description: ', '::: $D') for cl in c]
+        e = [dl.replace('authors: ', '::: $E') for dl in d]
+        f = [el.replace('date: ', '::: $F') for el in e]
         return f
     
     alph = replace_cats(clean)
@@ -54,8 +54,8 @@ def get_lines(file): # get data in form of lines
                 sorted_input_list.append(sort_sublist)
             return sorted_input_list
         
-        alphabetized_entries = sort_alph(split_entries)
-        return alphabetized_entries
+        #alphabetized_entries = sort_alph(split_entries)
+        return split_entries
 
     lines = split_n_sort(alph)
     return lines
@@ -66,11 +66,10 @@ def find_type(input_list, type_var, split=True): # collect items from entries th
         for entry in input_list:
             for string in entry:
                 if f'{type_var}' in string:
-                    replaced = string.replace(f'{type_var}','')
-                    stripped = replaced.strip()
-                    split = stripped.split()
-                    types.append(split)
-                    break
+                    replace = string.replace(f'{type_var}','')
+                    strip = replace.strip()
+                    splits = strip.split()
+                    types.append(splits)
         return types
     else:
         types = []
@@ -80,7 +79,6 @@ def find_type(input_list, type_var, split=True): # collect items from entries th
                     replaced = string.replace(f'{type_var}','')
                     stripped = replaced.strip()
                     types.append(stripped)
-                    break
         return types
 
 def make_pair(list_of_entries, first_term, second_term, split=True): # make pairs of two variables, can be whole thing or split into words
@@ -161,12 +159,12 @@ def findCL(list, paired=True): # find average character length of words in an en
 
 def write_file(file, list_data, format): # writes data to another file
     with open(f'{file}', 'w') as outfile:
-        if f'{format}' is 'tuples':
+        if f'{format}' == 'tuples':
             for tup in list_data:
                 for item in tup:
                     outfile.write(str(item[0]) + '\n')   
             print("Successfully created file :)")
-        if f'{format}' is 'strings':
+        if f'{format}' == 'strings':
             for item in list_data:
                 outfile.write(str(item)+'\n')
             print("Successfully created file :)")    
@@ -188,19 +186,19 @@ def stats(num_list): # takes a list of numbers and does statistics calculations
 
 def get_gen_summary(list_of_HL):
     hl_WC = findWC(list_of_HL, paired=False) # list of word counts of every headline
-    write_file('headlines_WC.txt', hl_WC, format='strings')
+    write_file('headlines_WC.txt', hl_WC, 'strings')
     hl_CL = findCL(list_of_HL, paired=False) # list of character length averages of every headline
-    write_file('headlines_CL.txt', hl_CL, format='strings')
+    write_file('headlines_CL.txt', hl_CL, 'strings')
     stats_WC = average(hl_WC)
     stats_CL = average(hl_CL)
     results = [('General WC:', [hl_WC, stats_WC]), ('General CL:', [hl_CL, stats_CL])]
-    print(results[0])
-    print(results[1])
+    #print(results[0])
+    #print(results[1])
     return results
 
 def get_cat_summary(list_of_entries, category):
     data = get_category(list_of_entries, f'{category}') # list of lists of strings (split HLs)
-    write_file(f'{category}_headlines.txt', data, format='strings') # writes new file
+    write_file(f'{category}_headlines.txt', data, 'strings') # writes new file
     data_WC = findWC(data, paired=False) # returns list of just values
     write_file(f'{category}_WC.txt', data_WC, format='strings')
     if len(data_WC) == 0:
@@ -208,33 +206,35 @@ def get_cat_summary(list_of_entries, category):
     avgWC = average(data_WC)
     stats_WC = stats(data_WC)
     data_CL = findCL(data, paired=False)
-    write_file(f'{category}_CL.txt', data_WC, format='strings')
+    write_file(f'{category}_CL.txt', data_WC, 'strings')
     if len(data_CL) == 0:
         print('error! something went wrong:(')
     avgCL = average(data_WC)
     stats_CL = stats(data_CL)
     result = [('WC:', [avgWC, stats_WC]), ('CL:', [avgCL, stats_CL])]
-    print(result[0])
-    print(result[1])
+    #print(result[0])
+    #print(result[1])
     return result
 
 def loop_cat_summary(list_of_entries, categories):
     categories = category_list
 
 ### LISTS OF THINGS:
-entries = get_lines('/Users/rena/Desktop/COURSES/LING 250/FINAL PROJECT/headlines.txt')
+entries = get_lines('/Users/rena/Desktop/COURSES/LING 250/FINAL PROJECT/DATA.txt')
 
 #### SETS OF VARIABLES ####
 links = find_type(entries, '$A')
 
 headlines = find_type(entries, '$B')
-write_file('headlines.txt', headlines, format='strings')
+write_file('HL.txt', headlines, 'strings')
 HLwords = combine(headlines) # all words in all headlines
+writtenHL = find_type(entries, '$B', split=False)
+write_file('headlines_clean.txt', writtenHL, 'strings')
 
-categories = find_type(entries, '$C')
-category_list = nltk.FreqDist(categories)
-print(category_list.most_common(5))
-category_list.tabulate()
+categories = set(find_type(entries, '$C', split=False))
+write_file('categories_real.txt', categories, 'strings')
+#category_list = nltk.FreqDist(categories)
+#print(category_list.most_common(5))
 
 descriptions = find_type(entries, '$D')
 authors = find_type(entries, '$E')
@@ -242,6 +242,9 @@ dates = find_type(entries, '$F')
                 
 #### PAIRS ####
 categorized_headlines = make_pair(entries, '$B', '$C')
+write_file('cat_HL.txt', categorized_headlines, 'strings')
+test_cat_HLs = make_pair(entries, '$B', '$C', split=True)  
+print('test:', test_cat_HLs[0][0])
 
 #### GENERAL DESC STATS ####
 general_stats = get_gen_summary(headlines)
@@ -258,3 +261,27 @@ general_stats = get_gen_summary(headlines)
 # variance: 0.8486685158900964
 # std dev: 0.9212320640805423
 # range: 36.0
+
+
+def looky_look(entries_paired, target):
+    captured = []
+    for entry in entries_paired:
+        if f'{target}' in entry[1]:
+            captured.append(entry)
+            print(f'"{target}" found!')
+    return captured
+
+test = looky_look(test_cat_HLs, 'Great')
+print(test)
+
+def find_all(entries_paired, target):
+    captured = []
+    for entry in entries_paired:
+        if f'{target}' in entry[1]:
+            captured.append(entry)
+    if len(captured) == 0:
+        print('None found :(')
+    else:
+        return captured
+    
+politics_test = find_all(categorized_headlines, 'POLITICS')
